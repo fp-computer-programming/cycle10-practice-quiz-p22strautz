169972d205c1d7959e2761606b7085c580a2dee4
p22strautz <p22strautz@fairfieldprep.org>
2022-02-01 21:39:58 -0500

    cycle 10 practice quiz

--- cycle-10_practice-quiz-2.py
+++ cycle-10_practice-quiz-2.py
@@ -1,9 +1,15 @@
 # Author: SCT (AMDG) 1/31/22
 
 def factorial(number):
-    for i in range(1,number + 1):
-       fact = fact*i
-       print(fact)
+    result = 1 # base value to add to
+    num = 1 # counter for loop
+    if number == 0:
+        result = 0 # sets cenario for 0
+    else:
+        while num <= number: # factorial sum cant go past itself
+            result = num * result # sets total = to the total * counter
+            num += 1 # counter
+    return result
 
-
-factorial(3)
+# test case
+print(factorial(3))
--- cycle-10_prctice-quiz-3.py
+++ cycle-10_prctice-quiz-3.py
@@ -1,1 +1,16 @@
 # Author: SCT (AMDG) 1/31/22
+
+def fibon(number): # def function
+    # Conditonals used to set base values for each input
+    if number == 1: # 1 will always be 0
+        return [0]
+    if number == 2: # 2 will alays equal 0,1
+        return [0,1]
+    fib = [0,1]
+    while number > len(fib): # if input is > than 2, continue with series of sum values
+        fib.append(fib[-1] + fib[-2])
+    return fib
+
+# test cases
+print(fibon(5))
+print(fibon(10))
